6465b6e688dc632322e4b986a9c0e4d8b832182f
Chris Scutcher <chris.scutcher@ninebysix.co.uk>
2013-06-09 01:46:13 +0100

    Added docstrings and cleaned code a little

--- showtransmission/showtransmission.py
+++ showtransmission/showtransmission.py
@@ -7,14 +7,13 @@ import sys
 import argparse
 
 import feedparser
-from magicargumentparser import MagicArgumentParser
 from transmission import TransmissionClient
 
 logger = logging.getLogger("showtransmission")
 
 
-
 class Episode(object):
+    """Store each episode retrieved from ShowRSS feed"""
     def __init__(self, feed_entry):
         self.published = feed_entry["published_parsed"]
         self.link = feed_entry["link"]
@@ -34,16 +33,19 @@ class Episode(object):
     VALID_FILE_CHARS = "-_.() %s%s" % (string.ascii_letters, string.digits)
 
     def dir_name(self):
+        """Convert the show name to string suitable for use in filename"""
         dir_name = self.show_name.replace(" ", "_")
         dir_name = ''.join(char for char in dir_name if char in self.VALID_FILE_CHARS)
         return dir_name
 
 
 class ShowTransmissionException(Exception):
+    """Used for exceptions generated from ShowTransmission object"""
     pass
 
 
 class SetLogLevelAction(argparse.Action):
+    """argparse action that handles setting log levels"""
     def __init__(self, *args, **kwargs):
         super(SetLogLevelAction, self).__init__(*args, **kwargs)
         self.nargs = 0
@@ -51,19 +53,32 @@ class SetLogLevelAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         logging.getLogger().setLevel(self.const)
 
+
 class ShowTransmission(object):
+    """
+    Object used to download ShowRSS feed and add each new magnet link to transmission via
+    transmission RPC api.
+    A config file is used to store already uploaded hashes.
+    TODO: Probably need to change this to be based on dates instead so the config doesn't grow too
+    massive. That or track last N hashes
+
+    """
     DEFAULT_CONFIG_LOCATION = os.path.expanduser("~/.showtransmission")
+
     def __init__(self):
         self.config_location = None
         self.log_level = logging.INFO
         self.rss_location = None
         self.transmission_rpc_url = None
         self.output_options = False
-        self.write_config = False
         self.hashes = set()
 
     @staticmethod
     def make_hash_set(value):
+        """
+        Used to create set from comma seperated hashes as passed on command line
+        Used in arg parser in ShowTransmission.parse_args
+        """
         hashes = set()
         for cs_hash in value.split(","):
             cs_hash.strip()
@@ -72,6 +87,10 @@ class ShowTransmission(object):
         return hashes
 
     def parse_args(self, args=None):
+        """
+        Configure this ShowTransmission object by reading args.
+        :param args: Arguments to pass. If not set then use sys.arv[1:]
+        """
         if args is None:
             args = sys.argv[1:]
 
@@ -91,7 +110,7 @@ class ShowTransmission(object):
                             dest="config_location",
                             default=self.DEFAULT_CONFIG_LOCATION,
                             help=("Config file location. Default: %s" %
-                                    (self.DEFAULT_CONFIG_LOCATION,)))
+                                  (self.DEFAULT_CONFIG_LOCATION,)))
 
         parser.add_argument("--rss-location", "-r",
                             dest="rss_location",
@@ -107,16 +126,20 @@ class ShowTransmission(object):
                             action="store_true",
                             default=False)
 
-        parser.add_argument("--write-config",
-                            dest="write_config",
-                            help="Write new config to config path when done",
-                            action="store_true",
-                            default=False)
+        parser.add_argument("--reset-hashes",
+                            dest="hashes",
+                            help="Reset all hashes",
+                            action="store_const",
+                            const=set())
 
         self.try_load_config(parser.parse_args(args).config_location)
         parser.parse_args(args=args, namespace=self)
 
     def try_load_config(self, config_path):
+        """
+        Try and load config from json file at config_path.
+        This overwrites any config attributes already set in this instance of ShowTransmission
+        """
         try:
             config_file = json.load(file(config_path, 'r'))
         except IOError:
@@ -125,16 +148,14 @@ class ShowTransmission(object):
         self.transmission_rpc_url = config_file.get("transmission_rpc_url", None)
         self.hashes = set(config_file.get("hashes", []))
 
-
-    def make_config_dict(self, include_hashes=True):
+    def make_config_json(self, include_hashes=True):
+        """Create json suitable for saving as config filei to preserve settings and hashes
+        in this instance of ShowTransmission"""
         config_dict = {"rss_location": self.rss_location,
                        "transmission_rpc_url": self.transmission_rpc_url}
         if include_hashes:
             config_dict["hashes"] = list(self.hashes)
-        return config_dict
-
-    def make_config_json(self, include_hashes=True):
-        return json.dumps(self.make_config_dict(include_hashes=include_hashes))
+        return json.dumps(config_dict)
 
     def process(self):
         """Parse feed and add each magnet to transmission"""
@@ -142,7 +163,6 @@ class ShowTransmission(object):
             print self.make_config_json()
             return
 
-        episodes = []
         logger.info("Connecting to transmission at '%s'" % (self.transmission_rpc_url))
         transmission = TransmissionClient(self.transmission_rpc_url)
         transmission.check_connection()
@@ -159,9 +179,8 @@ class ShowTransmission(object):
             else:
                 logger.debug("Ignoring episode for '%s' as hash is in set" % (str(episode),))
 
+        file(self.config_location, 'w').write(self.make_config_json())
 
-        if self.write_config:
-            file(self.config_location, 'w').write(self.make_config_json())
 
 def run_script(args=None):
     logging.basicConfig()
